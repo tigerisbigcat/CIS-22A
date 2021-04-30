c60896b622c9dafc11a036c324d1e820762c61a8
Hao <xiayushan2003@gmail.com>
2021-04-29 22:58:01 -0700

    Create Lab 0.cpp

---
+++ Lab00/Lab 0.cpp
@@ -0,0 +1,26 @@
+/*
+Lei Hao,lab 0, July 3rd 2019
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "NAME:\t";
+    cout << "Lei Hao\n";
+    cout << "E-MAIL:\t";
+    cout << "xiayushan2003@gmail.com\n";
+    cout << "MAJOR:\t";
+    cout << "Computer Science\n";
+    cout << "COMPUTER EXPERIENCE:\t";
+    cout << "none\n";
+    return 0;
+}
+
+/*
+NAME:	Lei Hao
+E-MAIL:	xiayushan2003@gmail.com
+MAJOR:	Computer Science
+COMPUTER EXPERIENCE:	none
+Program ended with exit code: 0
+*/
